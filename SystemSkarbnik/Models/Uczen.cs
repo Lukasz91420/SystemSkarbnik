7477bcff023c65a83ea334da39ed203d221d4591
Marian Kowalski <salaso4361@eazenity.com>
2023-11-28 09:49:10 +0100

    Dodaj pliki projektów.

---
+++ SystemSkarbnik/Models/Uczen.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemSkarbnik.Models
+{
+    public class Uczen
+    {
+        public int ID { get; set; }
+        public string Imię { get; set; }
+        public string Nazwisko { get; set; }
+        public int KlasaID { get; set; }
+        public Klasa? Klasa { get; set; } = null!;
+        public ICollection<ZbiorkaUczen> ZbiorkaUczens { get; } = new List<ZbiorkaUczen>();
+
+    }
+}
